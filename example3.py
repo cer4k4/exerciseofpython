010fd00f736ff9edea1c5dce5746239ff7303e2d
AkA(Ali karimi Ashini) <cer4k4@gmail.com>
2025-07-14 13:03:15 +0330

    add index and enumerate() method

--- example3.py
+++ example3.py
@@ -1,12 +1,7 @@
 nums = [0,1,1254,5]
-x = 1
-
-def giveIndex(x,nums):
-    for index in range(len(nums)):
-        if x == nums[index]:
-            return index
-    return -1
-
-found_index = giveIndex(x,nums)
+x = 1000
+found_index = -1
+for index,number in enumerate(nums):
+    if x == number:
+        found_index = index
 print(found_index)
-

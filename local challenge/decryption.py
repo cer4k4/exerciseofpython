91f4b80532019d399f0acd346534d06a6ed2a0ee
AkA(Ali karimi Ashini) <cer4k4@gmail.com>
2025-07-16 16:21:05 +0330

    edit and clean code

--- local challenge/decryption.py
+++ local challenge/decryption.py
@@ -3,11 +3,8 @@ import pymongo
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["local"]
 mycol = mydb["ascii"]
-
 giveCharacter = dict()
 
-for data in mycol.find({},{"_id":0}):
-    giveCharacter[data["ascii code"]] = data["symbol"]
 
 # Checker for that character Is number
 def isnumber(char):
@@ -17,24 +14,28 @@ def isnumber(char):
     except:
         pass
 
+
 # Decprit data with seprate "," and after that devide to 2 and give to character with ascii code
 def decrytion(data):
     temp = ""
     word = ""
-    for number in data:
-        if number != ",":
-           s = isnumber(number)
-           if s != None:
-               temp += str(s)
+    data += ","
+    for char in data:
+        if char != ",":
+            number = isnumber(char)
+            if number:
+                temp += str(number)
         else:
             iemp = int(temp) / 2
             word += giveCharacter[iemp]
             temp = ""
     return word
 
+
+for data in mycol.find({}, {"_id": 0}):
+    giveCharacter[data["ascii code"]] = data["symbol"]
+
 while True:
     print("Enter you're string to decrypt")
     word = input()
-    print("decripted ===>",decrytion(word),"<===","\n")
-
-    
+    print("decripted ===>", decrytion(word), "<===", "\n")
--- local challenge/encrypt.py
+++ local challenge/encrypt.py
@@ -1,28 +1,31 @@
-import pymongo
 import random
 import string
+import pymongo
+
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["local"]
 mycol = mydb["ascii"]
-
 giveASCIIcode = dict()
 
-for data in mycol.find({},{"_id":0}):
-    giveASCIIcode[data["symbol"]] = data["ascii code"]
 
-# Give ascii code and product to 2
 def encrytion(data):
     temp = ""
     for char in data:
         ascii = giveASCIIcode[char] * 2
         for itrator in str(ascii):
-            temp = temp + itrator +''.join([random.choice(string.ascii_letters) for _ in range(10)])    
-        temp+=","
-    return temp
+            temp = temp + itrator + \
+                ''.join([random.choice(string.ascii_letters)
+                        for _ in range(10)])
+        temp += ","
+    return temp[:-1]
 
+
+for data in mycol.find({}, {"_id": 0}):
+    giveASCIIcode[data["symbol"]] = data["ascii code"]
+# Give ascii code and product to 2
 
 while True:
     print("Enter you're word to encrypt")
     word = input()
     encrypted = encrytion(word)
-    print("encrypted ===>",encrypted,"<===","\n")
+    print("encrypted ===>", encrypted, "\n")

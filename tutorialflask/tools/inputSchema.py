4be3a82062dd04d890eb5e01fe6f0605c0d054e7
AkA(Ali karimi Ashini) <cer4k4@gmail.com>
2025-07-29 16:56:56 +0330

    add validation

--- tutorialflask/tools/inputSchema.py
+++ tutorialflask/tools/inputSchema.py
@@ -1,7 +1,4 @@
 from time import time
-#from config.configer import Configer
-
-#myConf = Configer()
 # Cerberus provides powerful yet simple and lightweight data validation functionality out of the box and is designed to be easily extensible, allowing for custom validation.
 
 
@@ -16,3 +13,19 @@ class InputSchema:
             "password": {'type':'string','required': True},
         }
         return userregistermodel
+    
+    @staticmethod
+    def get_user_or_delete_user():
+        uuid = {"uuid":{'type':'string','required': True,'regex': r'^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$'}}
+        return uuid
+    
+    @staticmethod
+    def get_users_with_filter():
+        pagination = {
+            "value":{'required': True},
+            "field":{'type':'string','required': True,'allowed': ['phone_number', 'name', 'age','']},
+            "size":{'type':'integer','default':50,'required': True},
+            "page":{'type':'integer','default':1,'required': True}            
+        }
+        return pagination    
+    
